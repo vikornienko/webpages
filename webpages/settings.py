35c50478488bca64082ad0cc31c11033d4f39437
vikornienko <vikornienko76@gmail.com>
2023-07-17 19:30:42 +0500

    Commit 002 Adding application.

--- webpages/settings.py
+++ webpages/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Project app
+    "appchallenges.apps.AppchallengesConfig",
 ]
 
 MIDDLEWARE = [
